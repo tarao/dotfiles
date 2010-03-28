772c97985b852d8abc8e45fb1dfce54aae0481a6
tarao <tarao.gnn@gmail.com>
2010-03-28 14:24:26 +0000

    Fix for latest vimpulse mode.

--- .emacs.d/dot/tarao.el
+++ .emacs.d/dot/tarao.el
@@ -142,9 +142,9 @@
   (kbd "C-w") 'kill-region)
 (define-key viper-insert-global-user-map
   (kbd "C-w") 'kill-region)
-(define-key viper-insert-global-user-map
+(define-key viper-insert-basic-map
   (kbd "C-p") nil)
-(define-key viper-insert-global-user-map
+(define-key viper-insert-basic-map
   (kbd "C-n") nil)
 
 ;; line number mode
