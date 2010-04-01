27c68570cf6dc236da5738a546d373f7bda804ea
tarao <tarao.gnn@gmail.com>
2010-04-01 06:44:20 +0000

    Settings for viper and vimpulse in a single file.

--- .emacs.d/dot/tarao.el
+++ .emacs.d/dot/tarao.el
@@ -104,25 +104,6 @@
 (setq skk-user-directory "~/.ddskk")
 (setq skk-use-viper t)
 
-;; viper minor mode
-(setq viper-custom-file-name (locate-library "dot/.viper"))
-(setq viper-mode t)
-(setq viper-toggle-key (kbd "C-x C-z"))
-(setq viper-ex-style-motion nil)
-(setq viper-ex-style-editing nil)
-(setq blink-matching-paren t)
-(require 'viper)
-(setq viper-vi-state-id "")
-(setq viper-insert-state-id "INSERT")
-(setq viper-visual-characterwise-state-id "VISUAL")
-(setq viper-visual-linewise-state-id "VLINE")
-(setq viper-visual-blockwise-state-id "VBLOCK")
-(setq viper-emacs-state-id "x")
-
-;; viper-mode patches
-(require 'hexl-viper-patch)
-(defadvice viper-maybe-checkout (around viper-dont-ask-checkout activate) nil)
-
 ;; vimpulse
 (load "dot/vimpulse")
 
