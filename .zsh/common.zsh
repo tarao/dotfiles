f54de9548fcbe1e52d9a49fa35609e394610d931
tarao <tarao.gnn@gmail.com>
2010-06-01 03:37:44 +0000

    No remote access on completion.

--- .zsh/common.zsh
+++ .zsh/common.zsh
@@ -40,6 +40,7 @@ zstyle ':completion:*' format '%F{white}%d%f'
 zstyle ':completion:*' group-name ''
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 zstyle ':completion:*' keep-prefix
+zstyle ':completion:*' remote-access false
 zstyle ':completion:*' completer _oldlist _complete _match _ignored \
     _approximate _list _history
 autoload -U compinit
