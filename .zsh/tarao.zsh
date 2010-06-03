279aed699bb93f3bc8bc179f1b825faaca4e0418
ina <ina@kuis.kyoto-u.ac.jp>
2010-06-03 15:54:26 +0900

    Check if keymap afu exists.

--- .zsh/tarao.zsh
+++ .zsh/tarao.zsh
@@ -1,3 +1,3 @@
 SCREEN_TITLE_CMD_ARG=(ssh -1 su -1 man -1 v -1)
 bindkey '^R' anything-history
-bindkey -M afu '^R' anything-history
+bindkey -M afu >/dev/null 2>&1 && bindkey -M afu '^R' anything-history
