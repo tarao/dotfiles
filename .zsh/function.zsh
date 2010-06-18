24d109921873bd8e75b9ce0a792af97838a1184a
tarao <tarao.gnn@gmail.com>
2010-06-18 15:24:27 +0000

    Go-up function.

--- .zsh/function.zsh
+++ .zsh/function.zsh
@@ -7,10 +7,27 @@ function snatch() {
                 p (int)dup2(\$1, 2)")
 }
 
+# Go up
+# see http://subtech.g.hatena.ne.jp/secondlife/20080604/1212562182
+function gu () {
+    if [[ -z "$1" ]]; then
+        # move to repository root
+        { [[ -n "$vcs" ]] && $0 "$.vcs" } ||
+        $0 Makefile || $0 Rakefile || cd ..
+        return
+    fi
+    local parent; parent='.'
+    local fname; fname="$1"
+    while [[ "${parent:a}" != "/" ]]; do
+        parent="$parent/.."
+        [[ -e "$parent/$fname" ]] && cd "$parent" && return
+    done
+    return 1
+}
+
 function watchdir () {
     if [[ "$1" != "" ]]; then
-        local dir="$1"
-        shift
+        local dir="$1"; shift
         if [[ -x "`which inotifywait`" ]]; then
             ls $dir
             while true; do
@@ -27,8 +44,7 @@ function watchdir () {
 # git-hg compatibility
 function git() {
     if [[ "$vcs" = 'hg' ]]; then
-        local args
-        args=`git2hg $@`
+        local args; args=`git2hg $@`
         hg ${=args}
     else
         env git $@
