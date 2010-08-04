6894404c7618abdd4853d8fdaa3765e2ae5f0837
tarao <tarao.gnn@gmail.com>
2010-08-04 13:00:40 +0000

    No vimpulse-surround-delete/change operators in visual mode.

--- .emacs.d/dot/tarao/50_vimpulse.el
+++ .emacs.d/dot/tarao/50_vimpulse.el
@@ -68,6 +68,8 @@
       woman-use-own-frame nil) ; don't create new frame for manpages
 (require 'vimpulse)
 (load "vimpulse-surround")
+(define-key vimpulse-visual-basic-map "d" 'vimpulse-delete)
+(define-key vimpulse-visual-basic-map "c" 'vimpulse-change)
 
 ;; ex-commands
 (setq my-viper-extra-ex-commands
