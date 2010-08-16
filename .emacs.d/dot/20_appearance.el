a3c42b5bb58ac366d2c9dd2d000773763c6f7023
tarao <tarao.gnn@gmail.com>
2010-08-16 16:26:37 +0000

    Customize line-wrap character.

--- .emacs.d/dot/20_appearance.el
+++ .emacs.d/dot/20_appearance.el
@@ -11,6 +11,23 @@
 (require 'end-mark)
 (unless window-system (global-end-mark-mode))
 
+;; line-wrap character
+(defface wrap-face
+  '((((class color) (min-colors 88) (background dark))
+     :foreground "aquamarine4")
+    (((class color) (min-colors 88) (background light))
+     :foreground "aquamarine2")
+    (((class color) (min-colors 16))
+     :foreground "DarkCyan")
+    (((class color) (min-colors 8))
+     :foreground "gray")
+    (((type tty) (class mono))
+     :inverse-video t))
+  "Face of the wrap."
+  :group 'convenience)
+(set-display-table-slot standard-display-table 'wrap
+                        (make-glyph-code #xbb 'wrap-face))
+
 ;; show fullwidth-spaces and tabs
 (require 'jaspace)
 (setq jaspace-highlight-tabs t)
