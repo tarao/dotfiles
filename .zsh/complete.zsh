c1612614ceb5ebf56d993099245fe16b0cced673
tarao <tarao.gnn@gmail.com>
2010-08-22 13:41:52 +0000

    Separate file.

---
+++ .zsh/complete.zsh
@@ -0,0 +1,89 @@
+# completion
+setopt   auto_list auto_param_slash list_packed rec_exact
+unsetopt list_beep
+zstyle ':completion:*' menu select
+zstyle ':completion:*' format '%F{white}%d%f'
+zstyle ':completion:*' group-name ''
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+zstyle ':completion:*' keep-prefix
+zstyle ':completion:*' remote-access false
+zstyle ':completion:*' completer _oldlist _complete _match _ignored \
+    _list _history
+autoload -U compinit
+compinit
+
+# incremental completion
+if [[ $ZSH_VERSION == (<5->|4.<4->|4.3.<10->)* ]]; then
+    function () { # precompile
+        local A
+        A=~/.zsh/modules/auto-fu/auto-fu.zsh
+        [[ -e "${A:r}.zwc" ]] && [[ "$A" -ot "${A:r}.zwc" ]] ||
+        zsh -c "source $A; auto-fu-zcompile $A ${A:h}" >/dev/null 2>&1
+    }
+    source ~/.zsh/modules/auto-fu/auto-fu; auto-fu-install
+    function zle-line-init () { auto-fu-init }
+    zle -N zle-line-init
+    zstyle ':auto-fu:highlight' input bold
+    zstyle ':auto-fu:highlight' completion fg=white
+    zstyle ':auto-fu:var' postdisplay ''
+    function afu+cancel () {
+        afu-clearing-maybe
+        ((afu_in_p == 1)) && { afu_in_p=0; BUFFER="$buffer_cur"; }
+    }
+    function bindkey-advice-before () {
+        local key="$1"
+        local advice="$2"
+        local widget="$3"
+        [[ -z "$widget" ]] && {
+            local -a bind
+            bind=(`bindkey -M main "$key"`)
+            widget=$bind[2]
+        }
+        local fun="$advice"
+        if [[ "$widget" != "undefined-key" ]]; then
+            local code=${"$(<=(cat <<"EOT"
+                function $advice-$widget () {
+                    zle $advice
+                    zle $widget
+                }
+                fun="$advice-$widget"
+EOT
+            ))"}
+            eval "${${${code//\$widget/$widget}//\$key/$key}//\$advice/$advice}"
+        fi
+        zle -N "$fun"
+        bindkey -M afu "$key" "$fun"
+    }
+    bindkey-advice-before "^G" afu+cancel
+    bindkey-advice-before "^[" afu+cancel
+    bindkey-advice-before "^J" afu+cancel afu+accept-line
+
+    # delete unambiguous prefix when accepting line
+    function afu+delete-unambiguous-prefix () {
+        local buf; buf="$BUFFER"
+        local bufc; bufc="$buffer_cur"
+        ((afu_in_p == 1)) && [[ "$buf[-1]" != ' ' && "$buf" != "$bufc" ]] && {
+            # there are more than one completion candidates
+            zle afu+complete-word
+            [[ "$buf" == "$BUFFER" ]] && {
+                # the completion suffix was an unambiguous prefix
+                afu_in_p=0; buf="$bufc"
+            }
+            BUFFER="$buf"
+        }
+    }
+    zle -N afu+delete-unambiguous-prefix
+    function afu-ad-delete-unambiguous-prefix () {
+        local afufun="$1"
+        local code; code=${"$(<=(cat <<EOT
+            zle afu+delete-unambiguous-prefix
+            __accepted
+EOT
+        ))"}
+        code=${functions[$afufun]/__accepted/$code}
+        eval "function $afufun () { $code }"
+    }
+    afu-ad-delete-unambiguous-prefix afu+accept-line
+    afu-ad-delete-unambiguous-prefix afu+accept-line-and-down-history
+    afu-ad-delete-unambiguous-prefix afu+accept-and-hold
+fi
