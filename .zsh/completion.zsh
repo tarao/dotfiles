0e3d6c1d74cf37f30c3a5d2710d3bf71d3b872d3
tarao <tarao.gnn@gmail.com>
2011-09-04 23:08:12 +0000

    Cache completion.

--- .zsh/completion.zsh
+++ .zsh/completion.zsh
@@ -10,6 +10,7 @@ zstyle ':completion:*' keep-prefix
 zstyle ':completion:*' remote-access false
 zstyle ':completion:*' completer _oldlist _complete _match _ignored \
     _list _history
+zstyle ':completion:*' use-cache yes
 zstyle ':completion:sudo:*' environ PATH="$SUDO_PATH:$PATH"
 autoload -U compinit
 compinit
