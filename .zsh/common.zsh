8ff3fa444af28d304643e585b8acb161cc7e14f9
tarao <tarao.gnn@gmail.com>
2011-09-10 12:20:46 +0000

    Use C-p/C-n for history search on emacs binding.

--- .zsh/common.zsh
+++ .zsh/common.zsh
@@ -15,6 +15,8 @@ zle -N history-beginning-search-backward-end history-search-end
 zle -N history-beginning-search-forward-end history-search-end
 bindkey "^P" history-beginning-search-backward-end
 bindkey "^N" history-beginning-search-forward-end
+bindkey -M emacs "^P" history-beginning-search-backward-end
+bindkey -M emacs "^N" history-beginning-search-forward-end
 
 # options
 ## Changing Directories
