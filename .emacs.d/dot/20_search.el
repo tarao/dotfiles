809f2f0a251c157b78657dc5a7a0c2caacb27049
tarao <tarao.gnn@gmail.com>
2012-04-09 13:57:12 +0000

    Use moccur function to ask directory.

--- .emacs.d/dot/20_search.el
+++ .emacs.d/dot/20_search.el
@@ -2,6 +2,6 @@
 (global-set-key (kbd "C-M-m") 'moccur-grep-find)
 
 (defun moccur-grep-find-region (beg end &optional dir)
-  (interactive "r\nDDirectory: ")
-  (unless dir (setq dir (file-name-directory (buffer-file-name))))
-    (moccur-grep-find dir (list (buffer-substring-no-properties beg end))))
+  (interactive "r")
+  (unless dir (setq dir (moccur-grep-read-directory)))
+  (moccur-grep-find dir (list (buffer-substring-no-properties beg end))))
