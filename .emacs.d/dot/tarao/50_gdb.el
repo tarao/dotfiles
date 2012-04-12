2af0f4110f0bc4bbef7ab0358e99138762e83acb
tarao <tarao.gnn@gmail.com>
2012-04-12 13:44:58 +0000

    GDB settings.

---
+++ .emacs.d/dot/tarao/50_gdb.el
@@ -0,0 +1,6 @@
+(setq gud-chdir-before-run nil)
+
+;; show current line of code
+(defadvice gdb-display-source-buffer
+  (after ad-hl-line-source-buffer (buffer) activate)
+  (with-current-buffer buffer (hl-line-mode 1)))
