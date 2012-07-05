49b23453198d7d400ccebb2af24762ab7410c886
tarao <tarao.gnn@gmail.com>
2012-07-05 07:58:30 +0000

    Add auto modes.

--- .emacs.d/init/sh.el
+++ .emacs.d/init/sh.el
@@ -4,3 +4,11 @@
                    sh-indentation 4
                    sh-indent-for-case-label 0
                    sh-indent-for-case-alt '+)))
+(setq auto-mode-alist
+      (append
+       '(("\\.sh$" . sh-mode)
+         ("\\.csh$" . sh-mode)
+         ("\\.tcsh$" . sh-mode)
+         ("\\.bash$" . sh-mode)
+         ("\\.zsh$" . sh-mode))
+       auto-mode-alist))
