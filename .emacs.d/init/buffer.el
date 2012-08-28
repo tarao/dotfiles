21b7e510930980644157672d68a38e9b1a73a1d3
ina <ina@kuis.kyoto-u.ac.jp>
2012-08-28 16:41:58 +0900

    Do not show trailing whitespace in Buffer-menu-mode.

--- .emacs.d/init/buffer.el
+++ .emacs.d/init/buffer.el
@@ -23,3 +23,9 @@
 ;; use directory name instead of <num>
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'forward)
+
+;; Buffer-menu
+(add-hook 'Buffer-menu-mode-hook
+          #'(lambda ()
+              (hl-line-mode 1)
+              (setq show-trailing-whitespace nil)))
