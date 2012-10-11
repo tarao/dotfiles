375e484a366d77e187a0ec480f6c129ae2c88d55
tarao <tarao.gnn@gmail.com>
2012-10-11 16:59:04 +0000

    Refactoring.

--- .emacs.d/init/window-system.el
+++ .emacs.d/init/window-system.el
@@ -21,31 +21,31 @@
   (setq-default default-frame-alist
                 (append frame-alist default-frame-alist)))
 
-(defvar window-system-configured nil)
-(defun setup-window-system-configuration ()
-  (when (and window-system (not window-system-configured))
-    ;; default font
-    (create-fontset-from-ascii-font "Menlo-14:weight=normal:slant=normal"
-                                    nil "menloja")
-    (set-fontset-font "fontset-menloja"
-                      'unicode
-                      (font-spec :family "Hiragino Kaku Gothic ProN")
-                      nil
-                      'append)
-    (setq face-font-rescale-alist '(("Hiragino.*" . 1.2)))
-    (setq default-font "fontset-menloja")
-    (set-default-font default-font)
-    (setq-default initial-frame-alist
-                  (append `((font . ,default-font)) initial-frame-alist))
-    (setq-default default-frame-alist
-                  (append `((font . ,default-font)) default-frame-alist))
-    ;; current frame
-    (set-frame-parameter (selected-frame) 'font default-font)
-    (setq window-system-configured t)))
+(defvar default-font nil)
+
+(defun setup-window-system-configuration (&optional frame)
+  (with-selected-frame (or frame (selected-frame))
+    (when window-system
+      ;; default font
+      (create-fontset-from-ascii-font "Menlo-14:weight=normal:slant=normal"
+                                      nil "menloja")
+      (set-fontset-font "fontset-menloja"
+                        'unicode
+                        (font-spec :family "Hiragino Kaku Gothic ProN")
+                        nil
+                        'append)
+      (setq face-font-rescale-alist '(("Hiragino.*" . 1.2)))
+      (setq default-font "fontset-menloja")
+      (set-default-font default-font)
+      (setq-default initial-frame-alist
+                    (append `((font . ,default-font)) initial-frame-alist))
+      (setq-default default-frame-alist
+                    (append `((font . ,default-font)) default-frame-alist))
+      ;; current frame
+      (set-frame-parameter (selected-frame) 'font default-font)
+      (remove-hook 'after-make-frame-functions
+                   #'setup-window-system-configuration))))
 
 (if window-system
     (setup-window-system-configuration)
-  (add-hook 'after-make-frame-functions
-            '(lambda (f)
-               (with-selected-frame f
-                 (setup-window-system-configuration)))))
+  (add-hook 'after-make-frame-functions #'setup-window-system-configuration))
