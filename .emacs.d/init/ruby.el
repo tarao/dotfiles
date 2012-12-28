8f3d47d7ffc60676b78dc6063ef5ef9e9b3ef29c
tarao <tarao.gnn@gmail.com>
2012-12-29 03:05:47 +0900

    Rewrite ruby settings.

--- .emacs.d/init/ruby.el
+++ .emacs.d/init/ruby.el
@@ -1,17 +1,9 @@
-(autoload 'ruby-mode "ruby-mode"
-  "Mode for editing ruby source files" t)
-(autoload 'rdoc-mode "rdoc-mode"
-  "Mode for editing rdoc files" t)
+(bundle rdoc-mode)
+(bundle inf-ruby
+  (add-hook 'ruby-mode-hook #'inf-ruby-keys))
+
 (setq auto-mode-alist
       (append '(("\\.rb$" . ruby-mode)
                 ("Rakefile$" . ruby-mode)
-                ("\\.rdoc$" . rdoc-mode)) auto-mode-alist)
-      interpreter-mode-alist (append '(("ruby" . ruby-mode))
-                                     interpreter-mode-alist))
-(autoload 'run-ruby "inf-ruby"
-  "Run an inferior Ruby process")
-(autoload 'inf-ruby-keys "inf-ruby"
-  "Set local key defs for inf-ruby in ruby-mode")
-(add-hook 'ruby-mode-hook
-          '(lambda ()
-             (inf-ruby-keys)))
+                ("\\.rdoc$" . rdoc-mode)) auto-mode-alist))
+(add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode))
