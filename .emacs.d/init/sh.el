2f5798a511f5e9aa285374fbff98c1b55d8b6819
tarao <tarao.gnn@gmail.com>
2012-12-29 02:23:57 +0900

    Settings take effect without using a hook.

--- .emacs.d/init/sh.el
+++ .emacs.d/init/sh.el
@@ -1,9 +1,7 @@
-(add-hook 'sh-mode-hook
-          '(lambda ()
-             (setq sh-basic-offset 4
-                   sh-indentation 4
-                   sh-indent-for-case-label 0
-                   sh-indent-for-case-alt '+)))
+(setq-default sh-basic-offset 4
+              sh-indentation 4
+              sh-indent-for-case-label 0
+              sh-indent-for-case-alt '+)
 (setq auto-mode-alist
       (append
        '(("\\.sh$" . sh-mode)
