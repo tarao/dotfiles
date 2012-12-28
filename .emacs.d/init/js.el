4a1df803c897f221d50268e66ba164ca943fdcf7
tarao <tarao.gnn@gmail.com>
2012-12-29 00:05:18 +0900

    Rewrite JavaScript settings with js-mode and jshint.

--- .emacs.d/init/js.el
+++ .emacs.d/init/js.el
@@ -1,63 +1,41 @@
+(eval-when-compile (require 'cl))
+
 (setq auto-mode-alist
       (append '(("\\.json$" . js-mode)
                 ("\\.js$" . js-mode))
-              auto-mode-alist)
-      js-indent-level 4
-      js-expr-indent-offset 4)
+              auto-mode-alist))
+(setq-default js-indent-level 4
+              js-expr-indent-offset 4)
 
-(defconst flymake-allowed-js-file-name-masks
-  '(("\\.json$" flymake-js-init)
-    ("\\.js$" flymake-js-init)))
-(defcustom flymake-js-detect-trailing-comma t nil :type 'boolean)
-(defvar flymake-js-err-line-patterns
-  '(("^\\(.+\\)\:\\([0-9]+\\)\: \\(SyntaxError\:.+\\)\:$" 1 2 nil 3)))
-(when flymake-js-detect-trailing-comma
-  (setq
-   flymake-js-err-line-patterns
-   (append
-    flymake-js-err-line-patterns
-    '(("^\\(.+\\)\:\\([0-9]+\\)\: \\(strict warning: trailing comma.+\\)\:$"
-       1 2 nil 3)))))
+(defvar flymake-jshint-command "jshint")
+(defvar flymake-jshint-command-args nil)
+(defvar flymake-jshint-config nil)
+(defconst flymake-js-err-line-patterns
+  '((".*: line \\([[:digit:]]+\\), col \\([[:digit:]]+\\), \\(.*\\)$"
+     nil 1 2 3)))
 
-(defvar flymake-js-e4x-enabled nil)
-(defvar flymake-js-use-strict t)
-(defun flymake-js-command-opt (ls)
-  (let ((base (cons "-C" ls)))
-    (let ((strct (if flymake-js-use-strict (cons "-s" base) base)))
-      (if flymake-js-e4x-enabled (cons "-x" strct) strct))))
-(defun flymake-js-e4x ()
-  (interactive)
-  (setq flymake-js-e4x-enabled (not flymake-js-e4x-enabled)))
-(defun flymake-js-strict ()
-  (interactive)
-  (setq flymake-js-use-strict (not flymake-js-use-strict)))
-
+(autoload 'flymake-init-create-temp-buffer-copy "flymake")
 (defun flymake-js-init ()
   (let* ((temp-file (flymake-init-create-temp-buffer-copy
                      'flymake-create-temp-inplace))
          (local-file (file-relative-name
                       temp-file
-                      (file-name-directory buffer-file-name))))
-    (list "js" (flymake-js-command-opt (list local-file)))))
-(defun flymake-js-load ()
-  (interactive)
-  ;; (defadvice flymake-post-syntax-check
-  ;;   (before flymake-force-check-was-interrupted)
-  ;;   (setq flymake-check-was-interrupted t))
-  ;; (ad-activate 'flymake-post-syntax-check)
-  (set (make-local-variable 'flymake-err-line-patterns)
-       flymake-js-err-line-patterns)
-  (flymake-mode t))
-(defadvice flymake-post-syntax-check
-  (before flymake-force-check-was-interrupted activate)
-  (setq flymake-check-was-interrupted t))
+                      (file-name-directory buffer-file-name)))
+         (args (list* local-file flymake-jshint-command-args)))
+    (when flymake-jshint-config
+      (let ((config (expand-file-name flymake-jshint-config)))
+        (setq args (list* "--config" config args))))
+    (list flymake-jshint-command args)))
 
-(setq flymake-allowed-file-name-masks
-      (append flymake-allowed-file-name-masks
-              flymake-allowed-js-file-name-masks))
+(eval-after-load 'flymake
+  '(progn
+     (push '("\\.json$" flymake-js-init) flymake-allowed-file-name-masks)
+     (push '("\\.js$" flymake-js-init) flymake-allowed-file-name-masks)))
 
 (add-hook 'js-mode-hook
-          '(lambda ()
-             (progn
-               (hs-enable)
-               (flymake-js-load))))
+          #'(lambda ()
+             (hs-enable)
+             (flymake-mode 1)
+             (when (boundp 'flymake-err-line-patterns)
+               (set (make-local-variable 'flymake-err-line-patterns)
+                    flymake-js-err-line-patterns))))
