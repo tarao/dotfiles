812442841e762d13351dbe969e909b6bbbb5c5c3
tarao <tarao.gnn@gmail.com>
2012-12-29 21:26:58 +0900

    Put # at the beginning of function symbols.

--- .emacs.d/init/xml.el
+++ .emacs.d/init/xml.el
@@ -14,11 +14,11 @@
 
 ;; use nxml-mode instead of sgml, xml or html mode.
 (mapc
- (lambda (pair)
-   (if (or (eq (cdr pair) 'xml-mode)
-           (eq (cdr pair) 'sgml-mode)
-           (eq (cdr pair) 'html-mode))
-       (setcdr pair 'nxml-mode)))
+ #'(lambda (pair)
+     (if (or (eq (cdr pair) 'xml-mode)
+             (eq (cdr pair) 'sgml-mode)
+             (eq (cdr pair) 'html-mode))
+         (setcdr pair 'nxml-mode)))
  magic-mode-alist)
 
 ;; template for XHTML
