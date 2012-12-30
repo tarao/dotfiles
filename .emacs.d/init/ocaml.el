508b415dc6a67e3abb26ca95d1340744e54aea6b
tarao <tarao.gnn@gmail.com>
2012-12-30 20:46:25 +0900

    Rewrite settings for OCaml.

--- .emacs.d/init/ocaml.el
+++ .emacs.d/init/ocaml.el
@@ -1,11 +1,3 @@
-(setq auto-mode-alist (cons '("\\.ml\\w?" . tuareg-mode) auto-mode-alist))
-(autoload 'tuareg-mode "tuareg" "Major mode for editing Caml code" t)
+(add-to-list 'auto-mode-alist '("\\.ml\\w?$" . tuareg-mode))
 (autoload 'camldebug "camldebug" "Run the Caml debugger" t)
 (autoload 'tuareg-run-caml "tuareg" "Run the Caml interactive compiler" t)
-
-;; font-lock
-(if (and (boundp 'window-system) window-system)
-    (when (string-match "XEmacs" emacs-version)
-      (if (not (and (boundp 'mule-x-win-initted) mule-x-win-initted))
-          (require 'sym-lock))
-      (require 'font-lock)))
