81982e83408b774be0316f7a845d101f2965a9e9
tarao <tarao.gnn@gmail.com>
2013-01-01 07:04:42 +0900

    Don't override html-mode and xml-mode.

--- .emacs.d/init/xml.el
+++ .emacs.d/init/xml.el
@@ -2,15 +2,13 @@
 (add-to-list 'auto-mode-alist
              (cons (concat "\\."
                            (regexp-opt
-                            '("xml" "xsd" "sch" "rng" "xslt"
+                            '("htm" "html" "xml" "xsd" "sch" "rng" "xslt"
                               "svg" "rss" "mxml") t)
                            "\\'")
                    'nxml-mode))
 (setq magic-mode-alist
       (cons '("<\\?xml " . nxml-mode)
             magic-mode-alist))
-(fset 'xml-mode 'nxml-mode)
-(fset 'html-mode 'nxml-mode)
 
 ;; use nxml-mode instead of sgml, xml or html mode.
 (mapc
