cbf9cb5d6732387e4d39e416aba4b4577e9abeae
tarao <tarao.gnn@gmail.com>
2013-01-15 22:21:39 +0900

    Get rid of pit.el. and ask password.

--- .emacs.d/init/hatena.el
+++ .emacs.d/init/hatena.el
@@ -1,16 +1,9 @@
-(bundle pit)
 (bundle hatena-markup-mode)
 (bundle hatena-multi-mode
   (setq-default hatena:mm:filetype-alist '((ocaml . tuareg))))
 (bundle hatena-diary
-  (setq-default hatena:d:major-mode 'hatena:markup-mode))
-
-(eval-after-load-compile 'hatena-diary
-  ;; load account information via pit
-  (let* ((spec '((username . "") (password . "")))
-         (config (pit/get 'hatena `(require ,spec))) (sym "hatena:%s"))
-    (mapc #'(lambda (k) (set (intern (format sym k)) (cdr (assq k config))))
-          (mapcar #'car spec)))
-
-  ;; multi-mode
-  (add-hook 'hatena:markup-mode-hook #'hatena:multi-mode))
+  (setq-default hatena:d:major-mode 'hatena:markup-mode
+                hatena:username "tarao")
+  (eval-after-load-compile 'hatena-diary
+    ;; multi-mode
+    (add-hook 'hatena:markup-mode-hook #'hatena:multi-mode)))
