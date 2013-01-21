1f7ba87c9cb9299042103e2616369c13973dde5e
tarao <tarao.gnn@gmail.com>
2013-01-21 09:53:39 +0900

    Automatically start textserver.

--- .emacs.d/init/textarea.el
+++ .emacs.d/init/textarea.el
@@ -1,3 +1,4 @@
-(bundle textarea)
+(bundle textarea
+  (add-hook 'textarea:local-mode-hook #'textarea:start-server))
 (eval-after-load-compile 'end-mark
   (add-to-list 'end-mark-mode-buffers-regexp "^\\*textarea\\*$"))
