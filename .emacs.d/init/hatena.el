deb58c24f3512b29eff7570d3fde8142d45fe6e5
tarao <tarao.gnn@gmail.com>
2013-02-02 10:22:55 +0900

    Do not highlight lines tail in hatena-markup-mode.

--- .emacs.d/init/hatena.el
+++ .emacs.d/init/hatena.el
@@ -5,4 +5,8 @@
   (setq-default hatena:d:major-mode 'hatena:markup-mode
                 hatena:username "tarao"))
 (eval-after-load-compile 'hatena-markup-mode
-  (add-hook 'hatena:markup-mode-hook #'hatena:multi-mode))
+  (add-hook 'hatena:markup-mode-hook #'hatena:multi-mode)
+  (add-hook 'hatena:markup-mode-hook
+            #'(lambda ()
+                (set (make-local-variable 'whitespace-style)
+                     (remove 'lines-tail whitespace-style)))))
