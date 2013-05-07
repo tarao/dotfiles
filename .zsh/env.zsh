af8bbab4c42a3a2e8d35013091864235ec77a0b7
tarao <tarao.gnn@gmail.com>
2013-05-07 19:26:12 +0900

    Refactoring.

--- .zsh/env.zsh
+++ .zsh/env.zsh
@@ -10,19 +10,21 @@ whence xsel >/dev/null && {
 }
 
 # PATH
-if [ "`id -u`" -eq 0 ]; then
-  PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-else
-  PATH="/usr/local/bin:/usr/bin:/bin:/usr/games"
-fi
-export PATH="$HOME/bin:$PATH"
-
-# ruby
-path=(
-    ~/.gem/ruby/*/bin(N-/)
-    $path
-)
-export PATH
+function _set_path_env() {
+    local su_path; su_path=(
+        {,/usr/local,/usr}/sbin(N-/)
+    )
+    local user_path; user_path=(
+        ~/bin
+        ~/.gem/ruby/*/bin(N-/) # ruby
+        {,/usr/local,/usr}/bin(N-/)
+        {/usr,/usr/local}/games(N-/)
+    )
+    path=($user_path $path)
+    [ "`id -u`" -eq 0 ] && path=($root_path $path)
+    typeset -gxU path
+}
+_set_path_env
 
 # perl
 function _set_perl_env() {
