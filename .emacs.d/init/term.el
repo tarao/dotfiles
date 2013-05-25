42274b0b96b0e16675e3cc693f0f6b272b60a746
tarao <tarao.gnn@gmail.com>
2013-05-26 03:36:32 +0900

    Follow fix for Emacs 24.3.

--- .emacs.d/init/term.el
+++ .emacs.d/init/term.el
@@ -3,12 +3,17 @@
 
 (eval-after-load-compile 'term+
   (require 'xterm-256color)
-  (setq ansi-term-color-vector
-        [unspecified
-         "black"   "#aa0000" "#00aa00" "#aa5500"
-         "#8c8ce8" "#aa00aa" "#00aaaa" "#aaaaaa"
-         "#555555" "#ff5555" "#55ff55" "#ffff55"
-         "#5555ff" "#ff55ff" "#55ffff" "white"])
+  (let ((vec [unspecified
+              "black"   "#aa0000" "#00aa00" "#aa5500"
+              "#8c8ce8" "#aa00aa" "#00aaaa" "#aaaaaa"
+              "#555555" "#ff5555" "#55ff55" "#ffff55"
+              "#5555ff" "#ff55ff" "#55ffff" "white"]))
+    (if (string< "24.2" emacs-version)
+        (dotimes (i 16)
+          (let ((face (elt ansi-term-color-vector (1+ i))))
+            (set-face-foreground face (elt vec (1+ i)))
+            (set-face-background face (elt vec (1+ i)))))
+      (setq ansi-term-color-vector vec)))
   (bundle! term+key-intercept)
   (bundle! term+mode)
   (eval-after-load-compile 'evil (bundle! term+evil))
