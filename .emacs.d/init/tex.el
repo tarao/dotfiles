67553e37f4b0c97a8ae325549a593053d5bd42ac
tarao <tarao.gnn@gmail.com>
2014-12-15 16:42:16 +0900

    Use pLaTeX related package only when `platex` command is installed.

--- .emacs.d/init/tex.el
+++ .emacs.d/init/tex.el
@@ -1,13 +1,14 @@
-(bundle auctex
-  :checkout "61fbbce678e4ad570672b50712f951dc1e6aa02d"
-  (defconst TeX-default-mode 'japanese-latex-mode)
-  (defconst TeX-mode-files '("\\.sty$" "\\.tex$"))
+(when (executable-find "platex")
+  (bundle auctex
+    :checkout "61fbbce678e4ad570672b50712f951dc1e6aa02d"
+    (defconst TeX-default-mode 'japanese-latex-mode)
+    (defconst TeX-mode-files '("\\.sty$" "\\.tex$"))
 
-  (dolist (ext TeX-mode-files)
-    (add-to-list 'auto-mode-alist (cons ext TeX-default-mode)))
+    (dolist (ext TeX-mode-files)
+      (add-to-list 'auto-mode-alist (cons ext TeX-default-mode)))
 
-  (setq-default japanese-TeX-command-default "pTeX"
-                japanese-LaTeX-command-default "pLaTeX")
+    (setq-default japanese-TeX-command-default "pTeX"
+                  japanese-LaTeX-command-default "pLaTeX")
 
-  (add-hook 'TeX-mode-hook #'turn-on-reftex)
-  (add-hook 'LaTeX-mode-hook #'turn-on-reftex))
+    (add-hook 'TeX-mode-hook #'turn-on-reftex)
+    (add-hook 'LaTeX-mode-hook #'turn-on-reftex)))
