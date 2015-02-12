4a719e1270937c10d2ae82f6a18a57157d0d465f
tarao <tarao.gnn@gmail.com>
2015-02-12 11:55:37 +0900

    Use typescript.

---
+++ .emacs.d/init/ts.el
@@ -0,0 +1,10 @@
+(defconst ts-mode-files '("\\.ts$"))
+
+(bundle typescript
+  (setq auto-mode-alist
+        (append (mapcar #'(lambda (x) (cons x 'typescript-mode)) ts-mode-files)
+                auto-mode-alist))
+  (add-hook 'typescript-mode-hook
+            #'(lambda ()
+                (hs-enable)
+                (auto-complete-mode))))
