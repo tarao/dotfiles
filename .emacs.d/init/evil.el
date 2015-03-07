6d88a4ab32e429e33ce9f78469498c0c37b4cd20
tarao <tarao.gnn@gmail.com>
2015-03-07 17:05:54 +0900

    Use `bound-and-true-p`.

--- .emacs.d/init/evil.el
+++ .emacs.d/init/evil.el
@@ -172,20 +172,20 @@ to next line."
     (around evil-update-buffer-local-cursor-color-in-insert-state activate)
     "Allow ccc to update cursor color only when we are in insert
   state and in `skk-j-mode'."
-    (when (and (eq evil-state 'insert) (boundp 'skk-j-mode) skk-j-mode)
+    (when (and (eq evil-state 'insert) (bound-and-true-p skk-j-mode))
       ad-do-it))
   (defadvice evil-refresh-cursor
     (around evil-refresh-cursor-unless-skk-mode activate)
     "Allow ccc to update cursor color only when we are in insert
   state and in `skk-j-mode'."
-    (unless (and (eq evil-state 'insert) (boundp 'skk-j-mode) skk-j-mode)
+    (unless (and (eq evil-state 'insert) (bound-and-true-p skk-j-mode))
       ad-do-it))
   (defadvice evil-ex-search-update-pattern
     (around evil-inhibit-ex-search-update-pattern-in-skk-henkan activate)
     "Inhibit search pattern update during `skk-henkan-mode'.
   This is reasonable since inserted text during `skk-henkan-mode'
   is a kind of temporary one which is not confirmed yet."
-    (unless (and (boundp 'skk-henkan-mode) skk-henkan-mode)
+    (unless (bound-and-true-p skk-henkan-mode)
       ad-do-it))
 
   ;; view mode
