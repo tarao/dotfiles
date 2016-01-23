a0023d0d9b058132e800c9d5cfd6a907e96b3b02
tarao <tarao.gnn@gmail.com>
2016-01-24 04:23:16 +0900

    Tweak CSS/JS indentation in template files.

--- .emacs.d/init/web.el
+++ .emacs.d/init/web.el
@@ -1,7 +1,9 @@
 (defconst web-mode-files
   '("/templates/.*\\.html" "\\.mustache$"))
 (bundle web-mode
-  (setq-default web-mode-markup-indent-offset 2)
+  (setq-default web-mode-markup-indent-offset 2
+                web-mode-css-indent-offset 2
+                web-mode-code-indent-offset 2)
   (setq auto-mode-alist
         (append (mapcar #'(lambda (x) (cons x 'web-mode)) web-mode-files)
                 auto-mode-alist)))
