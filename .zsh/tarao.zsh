b74de7ca3b6246d8856d4898a79ec040c1085982
tarao <tarao.gnn@gmail.com>
2016-05-16 16:17:09 +0900

    Use ghq.

--- .zsh/tarao.zsh
+++ .zsh/tarao.zsh
@@ -1,3 +1,5 @@
+bindkey '^]' _ghq_select
+
 SCREEN_TITLE_CMD_ARG+=(v -1 vi -1 git 2)
 [[ -n "$eterm_options[(r)term:*+]" ]] || bindkey '^R' anything-history
 
--- .zsh/zle.zsh
+++ .zsh/zle.zsh
@@ -1,1 +1,12 @@
 WORDCHARS=${WORDCHARS:s,/,,}
+
+function _ghq_select () {
+    local repository=$(ghq list -p | peco)
+    zle -I
+    zle -R -c
+    [[ -n "$repository" ]] && {
+        BUFFER="cd $repository"
+        zle accept-line
+    }
+}
+zle -N _ghq_select
