26ad2cf7c53838295e68e5b578a7ec905ce625cd
tarao <tarao.gnn@gmail.com>
2016-06-11 12:38:07 +0900

    Replace a major-mode for TypeScript.

--- .emacs.d/init/ts.el
+++ .emacs.d/init/ts.el
@@ -1,6 +1,6 @@
 (defconst ts-mode-files '("\\.ts$"))
 
-(bundle typescript
+(bundle typescript-mode
   (setq auto-mode-alist
         (append (mapcar #'(lambda (x) (cons x 'typescript-mode)) ts-mode-files)
                 auto-mode-alist))
