cc52d98eeb5be90420c59b15f60d7fdb9a61164d
tarao <tarao.gnn@gmail.com>
2018-12-03 15:22:56 +0900

    Temporarily disable multi-mode.

--- .emacs.d/init/hatena.el
+++ .emacs.d/init/hatena.el
@@ -1,6 +1,6 @@
 (bundle hatena-markup-mode)
 (with-eval-after-load-feature 'hatena-markup-mode
-  (add-hook 'hatena:markup-mode-hook 'hatena:multi-mode)
+  ;; (add-hook 'hatena:markup-mode-hook 'hatena:multi-mode)
   (add-hook 'hatena:markup-mode-hook
             #'(lambda ()
                 (set (make-local-variable 'whitespace-style)
