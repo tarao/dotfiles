58698af29a656e5f8b45449f173e96a816ee5b71
tarao <tarao.gnn@gmail.com>
2019-08-11 18:54:07 +0900

    Tweak limitations for syntax-highliting complex Scala code.

--- .emacs.d/init/basic.el
+++ .emacs.d/init/basic.el
@@ -6,7 +6,9 @@
 ;; shell
 (setq-default explicit-shell-file-name "zsh")
 (setq shell-file-name "zsh"
-      shell-command-switch "-c")
+      shell-command-switch "-c"
+      max-lisp-eval-depth 50000
+      max-specpdl-size 5000)
 
 ;; interactive
 (fset 'yes-or-no-p 'y-or-n-p)
