36d416016236d2e3735cd4a753aba2157ac5b6a8
tarao <tarao.gnn@gmail.com>
2019-08-11 18:55:10 +0900

    Make test result windows unsplitted.

--- .emacs.d/init/compilation.el
+++ .emacs.d/init/compilation.el
@@ -11,4 +11,11 @@
   (let ((inhibit-read-only t))
     (ansi-color-apply-on-region compilation-filter-start (point))))
 
+(defun compilation/maximize-test-window (&rest args)
+  (when (string-match "\\btest\\b" (buffer-name))
+    (message "buffer: %s" (buffer-name))
+    (with-current-buffer (buffer-name)
+      (delete-other-windows (get-buffer-window (buffer-name))))))
+
 (add-hook 'compilation-filter-hook #'compilation/colorize)
+(add-hook 'compilation-start-hook #'compilation/maximize-test-window)
