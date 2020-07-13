3cc7e6ed49d6591be2bd42233fea75d64f861510
tarao <tarao.gnn@gmail.com>
2020-07-13 17:41:30 +0900

    Make gopls installation asynchronous.

--- .emacs.d/init/golang.el
+++ .emacs.d/init/golang.el
@@ -4,10 +4,25 @@
 (bundle lsp-mode)
 (bundle lsp-ui)
 
-(add-hook 'go-mode-hook
-          '(lambda ()
-             (unless (executable-find "gopls")
-               (call-process-shell-command "go get golang.org/x/tools/gopls@latest"))
-             (add-hook 'before-save-hook 'lsp-format-buffer t t)
-             (add-hook 'before-save-hook 'lsp-organize-imports t t)
-             (lsp)))
+(defmacro with-gopls-installed (&rest body)
+  `(if (executable-find "gopls")
+       (progn
+         ,@body)
+     (let ((buf (current-buffer))
+           (proc-buf (get-buffer-create "*install-gopls*"))
+           (body ',body))
+       (make-process
+        :name "install-gopls"
+        :buffer proc-buf
+        :command (list "go" "get" "golang.org/x/tools/gopls")
+        :sentinel `(lambda (proc event)
+                     (when (and (eq (process-status proc) 'exit)
+                                (= 0 (process-exit-status proc)))
+                       (with-current-buffer ,buf
+                         ,@body)))))))
+
+(add-hook 'go-mode-hook '(lambda ()
+                           (with-gopls-installed
+                            (add-hook 'before-save-hook 'lsp-format-buffer t t)
+                            (add-hook 'before-save-hook 'lsp-organize-imports t t)
+                            (lsp))))
