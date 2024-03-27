e65293e4e59d7e883b274d650831959b60a2a459
tarao <tarao.gnn@gmail.com>
2024-03-27 17:27:14 +0900

    Use Google Cloud SDK.

--- .zsh/tarao.kafka.local.zsh
+++ .zsh/tarao.kafka.local.zsh
@@ -5,3 +5,10 @@ source ~/.zsh/emacs.tarao.kafka.local.zsh
 [ -f ~/.rye/env ] && {
     source ~/.rye/env
 }
+
+[ -f ~/.local/share/google-cloud-sdk/path.zsh.inc ] && {
+    . ~/.local/share/google-cloud-sdk/path.zsh.inc
+}
+[ -f ~/.local/share/google-cloud-sdk/completion.zsh.inc ] && {
+    . ~/.local/share/google-cloud-sdk/completion.zsh.inc
+}
