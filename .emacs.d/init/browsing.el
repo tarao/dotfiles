b3ebf0b248c45ae8ef6d1a52c4620fee3c0ed5d7
tarao <tarao.gnn@gmail.com>
2024-06-11 16:28:19 +0900

    Open browser from WSL.

--- .emacs.d/init/browsing.el
+++ .emacs.d/init/browsing.el
@@ -2,3 +2,15 @@
   (with-eval-after-load-feature 'browse-url
     (setq browse-url-generic-program "xdg-open"
           browse-url-browser-function 'browse-url-generic)))
+
+(defun wsl-browse-url (url &rest args)
+  (prog1 (message "Open %s" url)
+    (shell-command-to-string
+     (mapconcat #'shell-quote-argument
+                (list "cmd.exe" "/c" "start" url)
+                " "))))
+
+(when (getenv "WSL_DISTRO_NAME")
+  (with-eval-after-load-feature 'browse-url
+    (setq browse-url-browser-function #'wsl-browse-url)))
+
