b7ba97fde5dd57b0de5d6d831b3265098244035f
tarao <tarao.gnn@gmail.com>
2025-04-08 18:57:42 +0900

    Config to edit VBScript files.

---
+++ .emacs.d/init/vbs.el
@@ -0,0 +1,11 @@
+(bundle emacswiki:visual-basic-mode
+  (setq auto-mode-alist (append '(("\\.vbs" . visual-basic-mode)
+                                  ("\\.asp" . visual-basic-mode))
+                                auto-mode-alist))
+  (setq-default visual-basic-mode-indent 4)
+  (add-hook 'visual-basic-mode-hook
+            #'(lambda ()
+                (auto-complete-mode 1)
+                (undo-tree-mode 1)
+                (outline-indent-minor-mode 1)
+               )))
